22b60aba42aff27e69a8c79f8a71d4f3d6a9838a
steve62742 <steve62742@yahoo.gr>
2016-09-28 12:09:48 +0300

    package.json changes

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "materialcv",
+  "scripts": {
+    "start": "node server.js",
+    "postinstall": "bower install"
+  },
+  "version": "0.0.0",
+  "dependencies": {
+    "express": "*"
+  }
+}
